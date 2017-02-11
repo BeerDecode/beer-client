ec662a3fe1d3b64d06bf3e4ef596f34333592fae
Nolan Rigo <nolan.rigo@gmail.com>
2017-02-11 10:15:37 +0100

    Use react-router for navigation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router';
+
 import App from './components/App';
 import SearchPage from './components/SearchPage';
 import BeerPage from './components/BeerPage';
@@ -5,3 +9,13 @@ import E404Page from './components/E404Page';
 
 import './app.scss';
 
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRedirect to="/search" />
+      <Route path="search" component={SearchPage} />
+      <Route path="beer" component={BeerPage} />
+      <Route path="*" component={E404Page} />
+    </Route>
+  </Router>
+), document.getElementById('app'));
